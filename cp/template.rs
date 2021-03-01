2a69084b5bbb5489cd4d08fadebd58e1e32aff2f
Stephen Mwangi <st3v3n.mw@gmail.com>
2021-03-01 20:04:01 +0300

    moved things around

--- cp/template.rs
+++ cp/template.rs
@@ -1,17 +1,16 @@
 #![allow(unused)]
 use std::io::{self, prelude::*}; use std::str;
 
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout()); let mut sc = Scanner::new(stdin.lock()); let mut out = io::BufWriter::new(stdout.lock());
+    #[macro_export] macro_rules! cin { ($dt:ty) => (sc.token::<$dt>()); ($dt:ty, $n:expr) => { { let mut arr: [$dt; $n] = [0; $n]; for i in 0..$n { arr[i] = sc.token::<$dt>(); } arr } }; }
+
+    let t = cin!(i64);
+    for _ in 0..t {
+
+    }
+}
+
 #[macro_export] macro_rules! dbg { ($expr:expr) => { match $expr { expr => { eprintln!("{}: {} = {:#?}", line!(), stringify!($expr), &expr); expr } } }; ($l:literal, $expr:expr) => { match $expr { expr => { eprintln!(concat!(line!(), ": ", stringify!($expr), " = ", $l), &expr); ($l, expr) } } } }
 struct Scanner<R> { reader: R, buf_str: Vec<u8>, buf_iter: str::SplitWhitespace<'static>}
 impl<R: BufRead> Scanner<R> { fn new(reader: R) -> Self { Self { reader, buf_str: vec![], buf_iter: "".split_whitespace() } } fn token<T: str::FromStr>(&mut self) -> T { loop { if let Some(token) = self.buf_iter.next() { return token.parse().ok().expect("Failed parse"); } self.buf_str.clear(); self.reader.read_until(b'\n', &mut self.buf_str).expect("Failed read"); self.buf_iter = unsafe { let slice = str::from_utf8_unchecked(&self.buf_str); std::mem::transmute(slice.split_whitespace()) } } } }
-
-fn main() {
-    let (stdin, stdout) = (io::stdin(), io::stdout());
-    let mut sc = Scanner::new(stdin.lock()); let mut out = io::BufWriter::new(stdout.lock());
-    #[macro_export] macro_rules! cin { ($dt:ty) => (sc.token::<$dt>()); ($dt:ty, $n:expr) => { { let mut arr: [$dt; $n] = [0; $n]; for i in 0..$n { arr[i] = sc.token::<$dt>(); } arr } }; }
-
-    let t = cin!(u32);
-    for _ in 0..t {
-        dbg!(t);
-    }
-}
